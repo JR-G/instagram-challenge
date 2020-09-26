8081a2068c4489f2ccd12fd21704916c709ecaf0
JRG <47917431+JR-G@users.noreply.github.com>
2020-09-26 17:24:29 +0100

    Build: file upload service

---
+++ src/main/java/com/james/instagram/storage/StorageException.java
@@ -0,0 +1,12 @@
+package com.james.instagram.storage;
+
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
