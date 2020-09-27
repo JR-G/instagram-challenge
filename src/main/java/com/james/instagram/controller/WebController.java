238805fa26fba9e9d62779c7c17f3fd4bbe280a6
JRG <47917431+JR-G@users.noreply.github.com>
2020-09-27 15:22:13 +0100

    Build: added user sign in

--- src/main/java/com/james/instagram/controller/WebController.java
+++ src/main/java/com/james/instagram/controller/WebController.java
@@ -6,27 +6,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WebController {
 
-    @RequestMapping(value="/home")
+    @RequestMapping("/home")
     public String home() {
         return "home";
     }
 
-    @RequestMapping(value="/user")
+    @RequestMapping("/user")
     public String user(){
         return "user";
     }
 
-    @RequestMapping(value="/admin")
+    @RequestMapping("/admin")
     public String admin(){
         return "admin";
     }
 
-    @RequestMapping(value="/login")
+    @RequestMapping("/login")
     public String login(){
         return "login";
     }
 
-    @RequestMapping(value="/403")
+    @RequestMapping("/403")
     public String Error403(){
         return "403";
     }
---
+++ src/main/java/com/james/instagram/model/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.james.instagram.model;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableAutoConfiguration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+//                .passwordEncoder(new BCryptPasswordEncoder())
+                .usersByUsernameQuery("SELECT username, CONCAT('{noop}', password), enabled FROM users WHERE " +
+                        "username=?")
+                .authoritiesByUsernameQuery("SELECT username, role FROM user_roles WHERE username=?");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/home").permitAll().antMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
+                .permitAll();
+        http.exceptionHandling().accessDeniedPage("/403");
+    }
+}
