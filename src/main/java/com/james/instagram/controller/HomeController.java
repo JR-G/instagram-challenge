1754d4f84e04343b952e3cd7db9473f3ee18f685
JRG <47917431+JR-G@users.noreply.github.com>
2020-09-27 13:40:08 +0100

    Build: added html files for login

--- src/main/java/com/james/instagram/controller/HomeController.java
+++ src/main/java/com/james/instagram/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-	@RequestMapping(value = "/home")
+	@RequestMapping(value = "/feed")
 	public String index() {
 		return "index";
 	}
--- src/main/java/com/james/instagram/storage/StorageProperties.java
+++ src/main/java/com/james/instagram/storage/StorageProperties.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("storage")
 public class StorageProperties {
 
-    private String location = "uploaded-pics";
+    private String location = "images";
 
     public String getLocation() {
         return location;
